94fab62a1268ea5af5e0e1581e8684fa365b8666
Brandon Fitzgibbon <brandon_fitzgibbon@live.ca>
2024-05-27 16:47:15 -0400

    fix: solved day three part 1

--- daythree/main.go
+++ daythree/main.go
@@ -4,14 +4,74 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"strings"
 )
 
-func parseDigit(data []byte) (int, int) {
+const (
+	WIDTH = 141.0
+)
+
+type Container struct {
+	number int
+	nlen   int
+	data   []byte
+	part   bool
+	line   int
+	col    int
+}
+
+func (container Container) String() string {
+	sb := strings.Builder{}
+	span := container.nlen
+	if container.col == 1 || container.col+container.nlen == WIDTH {
+		span += 1
+	} else {
+		span += 2
+	}
+	sb.WriteString(fmt.Sprintf("\nNumber %d\nLength %d\nLine %d\nCol %d\nPart %t\nData:\n", container.number, container.nlen, container.line, container.col, container.part))
+	for i, v := range container.data {
+		sb.WriteByte(v)
+		if (i+1)%span == 0 {
+			sb.WriteRune('\n')
+		}
+	}
+	return sb.String()
+}
+
+func parseDigit(data []byte) int {
 	val := 0
 	for i, v := range data {
 		val += int(rune(v)-rune('0')) * int(math.Pow(10, float64(len(data)-i-1)))
 	}
-	return val, len(data)
+	return val
+}
+
+func getDataSlice(data []byte, row int, col int, len int) ([]byte, bool) {
+	part := false
+	slice := make([]byte, 0)
+	for i := -1; i < 2; i++ {
+		if row+i < 1 {
+			continue
+		}
+		if row+i > WIDTH-1 {
+			continue
+		}
+		for j := -1; j < len+1; j++ {
+			if col+j < 1 {
+				continue
+			}
+			if col+j > WIDTH-1 {
+				continue
+			}
+			ind := ((row - 1 + i) * WIDTH) + (col - 1 + j)
+			ele := data[ind]
+			if ele != '.' && !(ele >= '0' && ele <= '9') {
+				part = true
+			}
+			slice = append(slice, ele)
+		}
+	}
+	return slice, part
 }
 
 func main() {
@@ -19,74 +79,34 @@ func main() {
 	if err != nil {
 		return
 	}
-	plane := make([][]byte, 0)
-	currentLine := 0
-	for i, v := range data {
-		if v == '\n' {
-			plane = append(plane, data[currentLine:i])
-			currentLine = i + 1
-		}
-	}
-	sum := 0
-	total := 0
+	sb := strings.Builder{}
+	containers := make([]Container, 0)
 	digitStart := -1
-	for i := 0; i < len(plane); i++ {
-		row := plane[i]
-		for j := 0; j < len(row); j++ {
-			v := row[j]
-			if v >= '0' && v <= '9' && j+1 != len(row) {
-				if digitStart == -1 {
-					digitStart = j
-				}
-			} else {
-				if digitStart > -1 {
-					val, length := parseDigit(row[digitStart:j])
-					var startingRow, endingRow, startingColumn, endingColumn int
-					if i-1 > 0 {
-						startingRow = i - 1
-					} else {
-						startingRow = 0
-					}
-					if i+1 < len(plane) {
-						endingRow = i + 1
-					} else {
-						endingRow = i
-					}
-					if digitStart-1 >= 0 {
-						startingColumn = digitStart - 1
-					} else {
-						startingColumn = digitStart
-					}
-					if digitStart+length < len(row) {
-						endingColumn = digitStart + length
-					} else {
-						endingColumn = len(row) - 1
-					}
-					part := false
-					symbol := '.'
-					for k := startingRow; k <= endingRow; k++ {
-						checkRow := plane[k]
-						for l := startingColumn; l <= endingColumn; l++ {
-							checkItem := checkRow[l]
-							if checkItem >= 32 && checkItem <= 47 && checkItem != '.' {
-								part = true
-								symbol = rune(checkItem)
-								break
-							}
-						}
-						if part {
-							break
-						}
-					}
-					fmt.Println(startingRow+1, endingRow+1, startingColumn+1, endingColumn+1, i+1, j+1, val, part, (string)(symbol))
-					if part {
-						sum += val
-					}
-					total += val
-					digitStart = -1
-				}
+	for i, v := range data {
+		if v >= '0' && v <= '9' {
+			if digitStart == -1 {
+				digitStart = i
+			}
+		} else {
+			if digitStart > -1 {
+				number := parseDigit(data[digitStart:i])
+				len := i - digitStart
+				row := int(math.Ceil(float64(digitStart+1) / WIDTH))
+				col := (WIDTH) - ((row * WIDTH) - digitStart) + 1
+				container := Container{number: number, nlen: len, line: row, col: col}
+				container.data, container.part = getDataSlice(data, row, col, len)
+				containers = append(containers, container)
+				digitStart = -1
 			}
 		}
 	}
-	fmt.Println(sum, total)
+	sum := 0
+	for _, v := range containers {
+		sb.WriteString(fmt.Sprint(v))
+		if v.part {
+			sum += v.number
+		}
+	}
+	fmt.Println(sum)
+	os.WriteFile("./output.txt", []byte(sb.String()), 0777)
 }
